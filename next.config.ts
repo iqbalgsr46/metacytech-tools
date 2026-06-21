3b5ce97ae20c69b835ceaacc4c2387960d9787c6
Tubagus Iqbal Husaeni <ikbalgsr46@gmail.com>
2026-06-21 16:51:10 +0700

    fix: remove invalid turbo: false from next.config.ts (breaks build on all platforms)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Disable Turbopack - not supported on Android/ARM */
-  turbo: false,
 };
 
 export default nextConfig;
